f98353d28ec2ce00b2fb0059d3b4a6387477739a
pstupka <patryk.stupka@xperi.com>
2022-11-28 23:32:21 +0100

    Some stuff

--- source/globals/globals.gd
+++ source/globals/globals.gd
@@ -7,5 +7,5 @@ const NIGHT = 1
 const DAY_MODULATE := Color("#ffffff")
 const NIGHT_MODULATE := Color("#676767")
 
-const ROAD_MIN_POSITION := 400.0
-const ROAD_MAX_POSITION := 470.0
+const ROAD_MIN_POSITION := 410.0
+const ROAD_MAX_POSITION := 480.0
