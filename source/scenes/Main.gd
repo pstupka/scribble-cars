67afd420ae88ba5469157c89615a575876a1f6d7
pstupka <patryk.stupka@xperi.com>
2022-12-23 00:18:43 +0100

    Lightning tweaks

--- source/scenes/Main.gd
+++ source/scenes/Main.gd
@@ -4,7 +4,8 @@ var current_state = Globals.DAY setget set_state
 onready var apple_timer = $AppleTimer
 
 onready var background_modulate = $ParallaxBackground/CanvasModulate
-onready var foreground_modulate = $ParallaxBackground2/CanvasModulate2
+onready var road_modulate = $RoadParallax/CanvasModulate
+onready var foreground_modulate = $ParallaxBackground2/CanvasModulate
 onready var actors = $Actors
 
 func _ready():
@@ -24,10 +25,12 @@ func set_state(new_state):
 		Globals.DAY:
 			background_modulate.color = Globals.DAY_MODULATE
 			foreground_modulate.color = Globals.DAY_MODULATE
+			road_modulate.color = Globals.DAY_MODULATE
 			actors.modulate = Globals.DAY_MODULATE
 			
 		Globals.NIGHT:
 			background_modulate.color = Globals.NIGHT_MODULATE
+			road_modulate.color = Globals.NIGHT_MODULATE
 			foreground_modulate.color = Color("#565656")
 			actors.modulate = Globals.NIGHT_MODULATE
 	current_state = new_state
@@ -37,6 +40,7 @@ func set_state(new_state):
 func _on_AppleTimer_timeout():
 	var trees = get_tree().get_nodes_in_group("tree")
 	trees[randi() % trees.size()].make_fruit()
+
 
 func _on_time_of_day_changed(state):
 	match state:
