3030573ad181875312e0d2e6804d818714dff63c
pstupka <patryk.stupka@xperi.com>
2023-01-15 00:20:55 +0100

    Functional random cars spawning

--- source/scenes/Main.gd
+++ source/scenes/Main.gd
@@ -10,9 +10,7 @@ onready var player = $Actors/Player
 
 export var lanes_y_position = [410, 460]
 
-onready var car_templates = [
-	preload("res://source/scenes/actors/police_car.tscn")
-]
+onready var random_car = preload("res://source/scenes/actors/random_car.tscn")
 
 func _ready():
 	randomize()
@@ -49,10 +47,11 @@ func _on_time_of_day_changed(state):
 
 
 func _on_CarSpawnTimer_timeout():
-	var car_instance = car_templates[0].instance()
+	var car_instance = random_car.instance()
 	var lane = stepify(randf(),1)
 	actors.add_child(car_instance)
 	car_instance.global_position = Vector2(player.global_position.x - (2*lane - 1)*1500, lanes_y_position[lane])
 	car_instance.direction = Vector2(2*lane - 1 , 0)
 	
+	$CarSpawnTimer.wait_time = rand_range(1.44, 2.51)
 	
---
+++ source/scenes/actors/random_car.gd
@@ -0,0 +1,54 @@
+extends KinematicBody2D
+
+
+var screen_exit_delayed = false
+
+export var speed = 50
+var direction := Vector2.LEFT setget set_direction
+var velocity
+var car = null
+
+func _ready():
+	randomize()	
+	var new_car = Globals.car_templates[randi() % Globals.car_templates.size()].instance()
+	add_child(new_car)
+	car = new_car
+	car.scale.x = -direction.x
+	car.color = Globals.available_colors[randi() % Globals.available_colors.size()]
+	var animation =  car.animation_player.get_animation("move")
+	car.animation_player.connect("animation_finished", self, "_on_car_animation_finished")
+	car.animation_player.play("move")
+
+func _physics_process(_delta) -> void:
+	velocity = move_and_slide(car.speed * direction)
+
+
+func set_direction(new_direction):
+	if not car: return
+	car.scale.x = -new_direction.x
+	direction.x = new_direction.x
+
+
+
+func _on_car_animation_finished(anim_name: String):
+	car.animation_player.play("move")
+		
+
+
+func _on_screen_entered() -> void:
+	screen_exit_delayed = false
+	$VisibilityNotifier2D/ScreenExitedDelay.start()
+
+
+func _on_screen_exited():
+	if screen_exit_delayed:
+		call_deferred("queue_free")
+
+
+func _on_CarDiscoverArea_body_entered(body):
+	if body.is_in_group("player"):
+		car.jump()
+
+
+func _on_ScreenExitedDelay_timeout() -> void:
+	screen_exit_delayed = false
