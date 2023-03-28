0fd51c8d506f14580a7ad434f1d6b475ae595500
Patryk Stupka <patryk.stupka@xperi.com>
2023-03-28 13:17:25 +0200

    Add hair color to some pedestrians

--- source/scenes/actors/npc/pedestrian.gd
+++ source/scenes/actors/npc/pedestrian.gd
@@ -40,6 +40,13 @@ var npc_types := {
 	"res://assets/sprites/npc/human8.png": "human",
 	"res://assets/sprites/npc/human9.png": "human",
 	"res://assets/sprites/npc/human10.png": "human",
+	"res://assets/sprites/npc/human11.png": "human",
+	"res://assets/sprites/npc/human12.png": "human",
+	"res://assets/sprites/npc/human13.png": "human",
+	"res://assets/sprites/npc/human14.png": "human",
+	"res://assets/sprites/npc/human15.png": "human",
+	"res://assets/sprites/npc/human16.png": "human",
+	"res://assets/sprites/npc/human17.png": "human",
 }
 
 func _ready() -> void:
