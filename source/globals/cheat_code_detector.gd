60940599f4c7af95b0d09df2c8ddc5ccc8e0ccbf
Patryk Stupka <patryk.stupka@gmail.com>
2023-03-29 21:44:05 +0200

    Tweak cheat detector

--- source/globals/cheat_code_detector.gd
+++ source/globals/cheat_code_detector.gd
@@ -36,7 +36,7 @@ func _ready() -> void:
 	typing_timer.connect("timeout", self, "_on_typing_timer_timeout")
 	
 
-func _input(event: InputEvent) -> void:	
+func _input(event: InputEvent) -> void:
 	if event as InputEventJoypadButton:
 		for action in cheat_input_actions.keys():
 			if event.is_action_pressed(action):
@@ -55,11 +55,29 @@ func validate_cheat(cheat: String) -> void:
 		emit_signal("cheat_detected", codes[cheat])
 		match codes[cheat]:
 			"darek_mode": 
-				print("Darek moode activated")
+				print("Darek mode activated")
 				var darek_texture = load("res://assets/sprites/npc/darek.png")
+				
 				for pedestrian in get_tree().get_nodes_in_group("pedestrian"):
 					pedestrian.sprite.texture = darek_texture
 					pedestrian.sprite.position.y = -darek_texture.get_size().y / 4
+				
+				for player in get_tree().get_nodes_in_group("player"):
+					var darek_sprite = Sprite.new()
+					darek_sprite.texture = darek_texture
+					get_tree().current_scene.add_child(darek_sprite)
+
+					darek_sprite.global_position = player.global_position
+					var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+					var camera = player.get_node("Camera2D")
+					
+					tween.tween_property(darek_sprite, "rotation_degrees", 760.0, 1.5)
+					tween.parallel().tween_property(darek_sprite, "scale", Vector2(10.0, 10.0), 1.8)
+					tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+					tween.parallel().tween_property(camera, "offset_h", 0.0, 1.0).from(randf()*4)
+					tween.parallel().tween_property(camera, "offset_v", 0.0, 1.0).from(-randf()*4)
+					
+					tween.tween_callback(darek_sprite, "call_deferred", ["queue_free"])
 
 
 func _on_typing_timer_timeout() -> void:
