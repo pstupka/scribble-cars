84e2054b87d83ccbae252bd51c3718ba28d69b9f
Patryk Stupka <patryk.stupka@xperi.com>
2023-05-09 13:13:48 +0200

    More stars tweaking and add enter tweener to space

--- source/scenes/levels/Space.gd
+++ source/scenes/levels/Space.gd
@@ -1,14 +1,18 @@
 extends Node2D
-onready var stars_bg: ParallaxLayer = $ParallaxBackground/Stars
+
+
+onready var stars = $ParallaxBackground/Stars/Stars
+
 onready var player = $Actors/Rocket
 onready var collectables: Array
 
 onready var star_scene := preload("res://source/scenes/props/space/collectable_star.tscn")
+onready var enter_tweener = $EnterTweener
 
 export var scene_type = "space"
 
 func _ready() -> void:
-	for star in stars_bg.get_children():
+	for star in stars.get_children():
 		star.modulate = Color(randf(), 0.0, 0.0)
 		star.scale = star.scale * (randf() * 0.6 + 0.4)
 
@@ -17,11 +21,20 @@ func _ready() -> void:
 		collectables.append(new_star)
 		$Collectables.add_child(new_star)
 		new_star.global_position = Vector2(randf() * 4000 - 2000, randf() * 4000 - 2000)
-	
+		if new_star.global_position.length() < 100.0:
+			new_star.global_position.x += sign(new_star.global_position.x) * 200.0
+			new_star.global_position.y += sign(new_star.global_position.y) * 200.0
+
+
 	for asteroid in $Actors/Asteroids.get_children():
 		asteroid.player_rocket = player
 	
 	Globals.score = 0
+	
+	yield(get_tree().create_timer(0.1),"timeout")
+	get_tree().paused = true
+	var _err = enter_tweener.connect("enter_tween_completed", self, "_on_enter_tween_completed")
+	enter_tweener.apply_tween()
 
 
 func _process(_delta: float) -> void:
@@ -30,3 +43,6 @@ func _process(_delta: float) -> void:
 			Vector2(wrapf(collectable.global_position.x, player.global_position.x - 2000, player.global_position.x + 2000),\
 			wrapf(collectable.global_position.y, player.global_position.y - 2000, player.global_position.y + 2000))
 
+
+func _on_enter_tween_completed():
+	get_tree().paused = false
