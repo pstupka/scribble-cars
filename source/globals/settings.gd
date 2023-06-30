d2c69881d2fde2e044fc19ab74799bd6a83bf3be
Patryk Stupka <patryk.stupka@xperi.com>
2023-06-30 23:22:13 +0200

    More settings wip

--- source/globals/settings.gd
+++ source/globals/settings.gd
@@ -9,13 +9,12 @@ enum TEXTURE_QUALITY {LOW, HIGH}
 var settings_dict = {
 	"sfx_mute": false,
 	"music_mute": false,
-	"sound_volume": 50,
+	"sound_volume": -6,
 	"fullscreen": false,
 	"on_screen_controls_visible": false,
 	"vibrations_enabled": true,
 	"texture_quality": TEXTURE_QUALITY.HIGH,
 }
-
 
 func _ready():
 	load_settings()
@@ -37,9 +36,16 @@ func set(key, value):
 	settings_dict[key] = value
 	match key:
 		"music_mute":
-			AudioServer.set_bus_mute(1, settings_dict["music_mute"])
-
-	emit_signal("setting_changed", key, value)
+			AudioServer.set_bus_mute(1, settings_dict[key])
+		"sfx_mute":
+			AudioServer.set_bus_mute(2, settings_dict[key])
+		"sound_volume":
+			AudioServer.set_bus_volume_db(0, settings_dict[key])
+		"fullscreen":
+			OS.window_fullscreen = settings_dict[key]
+		"texture_quality":
+			VisualServer.texture_set_shrink_all_x2_on_set_data(settings_dict[key])
+	emit_signal("setting_changed", key, settings_dict[key])
 
 
 func load_settings() -> void:
@@ -47,10 +53,11 @@ func load_settings() -> void:
 	if f.file_exists(settings_file):
 		f.open_encrypted_with_pass(settings_file, File.READ, OS.get_unique_id())
 		for key in settings_dict.keys():
-			settings_dict[key] = f.get_var()
+			set(key, f.get_var())
 			print(key + ": " + str(settings_dict[key]) + " loaded")
 		f.close()
-
+	else: 
+		save_settings()
 
 func save_settings() -> void:
 	var f = File.new()
--- source/scenes/menu/settings_menu.gd
+++ source/scenes/menu/settings_menu.gd
@@ -1,6 +1,12 @@
 extends Control
 
 onready var pik_sfx = $PikSfx
+onready var mute_music_button = $"%MuteMusicButton"
+onready var mute_sfx_button = $"%MuteSFXButton"
+onready var volume_slider = $"%VolumeSlider"
+onready var fullscreen_button = $"%FullscreenButton"
+
+
 
 var tween: SceneTreeTween
 var _close_guard = false
@@ -12,7 +18,8 @@ func _ready():
 	tween.tween_property(self, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.4) \
 		 .from(Color(1.0, 1.0, 1.0, 0.0))
 
-	$TestMute.set_pressed_no_signal(Settings.get("music_mute"))
+	set_initial()
+	register_controls()
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -24,6 +31,16 @@ func _notification(what):
 		_on_ExitButton_pressed()
 	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
 		_on_ExitButton_pressed()
+
+
+func set_initial() -> void:
+	mute_music_button.set_pressed_no_signal(Settings.get("music_mute"))
+	mute_sfx_button.set_pressed_no_signal(Settings.get("sfx_mute"))
+	volume_slider.value = db2linear(Settings.get("sound_volume"))
+	fullscreen_button.set_pressed_no_signal(Settings.get("fullscreen"))
+
+func register_controls() -> void:
+	volume_slider.connect("value_changed", self, "_on_volume_changed")
 
 
 func close_menu() -> void:
@@ -47,5 +64,17 @@ func _on_ExitButton_pressed():
 	close_menu()
 
 
-func _on_TestMute_toggled(button_pressed):
+func _on_volume_changed(value) -> void:
+	Settings.set("sound_volume", linear2db(value))
+
+
+func _on_MuteButton_toggled(button_pressed):
 	Settings.set("music_mute", button_pressed)
+
+
+func _on_FullscreenButton_toggled(button_pressed):
+	Settings.set("fullscreen", button_pressed)
+
+
+func _on_MuteSFXButton_toggled(button_pressed):
+	Settings.set("sfx_mute", button_pressed)
